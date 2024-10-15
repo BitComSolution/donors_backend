a794998ab501c9eecee9aaaf6eddb7fb5dfade42
Vadim Bobrov <vadim762905@gmail.com>
2024-10-15 21:30:24 +0700

    modify docker

--- .docker/deploy/build.sh
+++ .docker/deploy/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --no-cache
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build --no-cache
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 sleep 20
+docker-compose exec auth-app composer install --ignore-platform-reqs --no-dev -a
+docker-compose exec auth-app php artisan route:cache
+docker-compose exec auth-app php artisan config:clear
+docker-compose exec auth-app php artisan migrate
 
-docker-compose exec app composer install --ignore-platform-reqs --no-dev -a
-docker-compose exec app php artisan route:cache
-docker-compose exec app php artisan config:clear
-docker-compose exec app php artisan migrate
-
-#docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build app
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build auth-app
--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,36 @@
 FROM dunglas/frankenphp
 
+RUN install-php-extensions \
+    pcntl \
+    pdo_pgsql \
+    pgsql \
+    redis \
+    rdkafka \
+    zip \
+    ctype \
+    curl \
+    dom \
+    fileinfo \
+    filter \
+    hash \
+    mbstring \
+    openssl \
+    pcre \
+    session \
+    tokenizer \
+    xml
 
+RUN apt-get update && apt-get install -y \
+    supervisor \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+COPY .docker/supervisord/prod/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
 COPY . /app
 
 RUN rm -Rf tests/
 
+ENTRYPOINT ["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
